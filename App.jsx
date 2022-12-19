a25fa8017fe880d7451790894b3a6db7f05b25b2
ymc182 <ymc182@gmail.com>
2022-12-20 05:57:57 +1100

    css

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import "regenerator-runtime/runtime";
 import React from "react";
-
+import img from "./assets/logo-white.svg";
 import "./assets/global.css";
 import { useNearContext } from "./src/context/nearContext";
 import LoginBtn from "./src/components/LoginBtn";
@@ -9,9 +9,15 @@ export default function App() {
 	const { modal } = useNearContext();
 	return (
 		<>
-			<LoginBtn />
-			<main>
-				<h1></h1>
+			<LoginBtn
+				style={{
+					position: "absolute",
+					top: "1rem",
+					right: "1rem",
+				}}
+			/>
+			<main style={{ display: "flex", placeContent: "center" }}>
+				<img src={img} width="50%" alt="logo" />
 			</main>
 		</>
 	);
--- src/components/LoginBtn.js
+++ src/components/LoginBtn.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useNearContext } from "../context/nearContext";
 
-export default function LoginBtn() {
+export default function LoginBtn({ style }) {
 	const { modal, signOut, accountId } = useNearContext();
 
 	if (!accountId) {
-		return <button onClick={() => modal.show()}>Login</button>;
+		return (
+			<button style={style} onClick={() => modal.show()}>
+				Login
+			</button>
+		);
 	} else {
-		return <button onClick={signOut}>Logout</button>;
+		return (
+			<button style={style} onClick={signOut}>
+				Logout
+			</button>
+		);
 	}
 }
